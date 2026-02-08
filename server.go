608f48aab68f065ddcee0a22faf398e51f7609f5
Yashwant <yashwantkrsingh1997@gmail.com>
2026-02-08 17:27:14 +0530

    first commit

---
+++ server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+type SocialLink struct {
+	URL   string
+	Label string
+	Icon  template.HTML
+}
+
+type PageData struct {
+	Hero        string
+	SocialLinks []SocialLink
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.RequestLogger())
+
+	tmpl := template.Must(template.ParseGlob("templates/*.html"))
+	template.Must(tmpl.ParseGlob("templates/components/*.html"))
+	template.Must(tmpl.ParseGlob("templates/pages/*.html"))
+
+	e.Renderer = &echo.TemplateRenderer{Template: tmpl}
+
+	e.Static("/static", "static")
+
+	e.GET("/", func(c *echo.Context) error {
+		data := PageData{
+			Hero: "Hey, I'm Yashwant Kumar",
+			SocialLinks: []SocialLink{
+				{
+					URL:   "https://stackoverflow.com/users/9603922/yashwant-kumar",
+					Label: "Stack Overflow",
+					Icon:  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120" fill="currentColor"><path d="M84.4 93.8V70.6h7.7v30.9H22.6V70.6h7.7v23.2z"/><path d="M38.8 68.4l37.8 7.9 1.6-7.6-37.8-7.9-1.6 7.6zm5-18.1l35 16.3 3.2-7-35-16.4-3.2 7.1zm9.7-17.2l29.7 24.7 4.9-5.9-29.7-24.7-4.9 5.9zm19.2-18.3l-6.2 4.6 23 31 6.2-4.6-23-31zM38 86h38.6v-7.7H38V86z"/></svg>`,
+				},
+				{
+					URL:   "https://www.linkedin.com/in/yashwant-kumar-00b20b83/",
+					Label: "LinkedIn",
+					Icon:  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>`,
+				},
+				{
+					URL:   "https://github.com/namespace7",
+					Label: "GitHub",
+					Icon:  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>`,
+				},
+			},
+		}
+		return c.Render(http.StatusOK, "layout", data)
+	})
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
